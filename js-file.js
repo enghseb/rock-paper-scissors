ed1ce3897ff7aac1c06503bda4cbcef79c298b40
Sebastian <enghseb@gmail.com>
2022-07-01 19:24:28 +0000

    Making sure changes have been commited

--- js-file.js
+++ js-file.js
@@ -53,41 +53,56 @@ function playRound(computerPlay, userPlay){
 function game(){
     let playerScore = 0
     let computerScore = 0
-  
-    for (let rounds = 0; rounds < 5; rounds++) {
-        roundResult = playRound(computerPlay(), userPlay())
-        if (roundResult == 0){
-            playerScore++;
+    /* if(playerSelection != "") { */
+        for (let rounds = 0; rounds < 5; rounds++) {
+            roundResult = playRound(computerPlay(), userPlay())
 
-            const playerWonText = document.createElement('p');
-            playerWonText.classList.add('playerWonText')
-            playerWonText.textContent = "You won and scored a point.";
-            resultOutput.appendChild(playerWonText);
-
-            /* console.log("You won and scored a point.") */
-        } else if (roundResult == 1){
-            const playerDrewText = document.createElement('p');
-            playerDrewText.classList.add('playerDrewText')
-            playerDrewText.textContent = "You drew.";
-            resultOutput.appendChild(playerDrewText);
-
-            /* console.log("You drew.") */
-        } else {
-            computerScore++
-            const playerLostText = document.createElement('p');
-            playerLostText.classList.add('playerLostText')
-            playerLostText.textContent = "You lost.";
-            resultOutput.appendChild(playerLostText);
-
-            /* console.log("The computer won and scored a point.") */
+            if (roundResult == 0){
+                playerScore++;
+    
+                const playerWonText = document.createElement('p');
+                playerWonText.classList.add('playerWonText')
+                playerWonText.textContent = "You won and scored a point.";
+                resultOutput.appendChild(playerWonText);
+    
+                /* console.log("You won and scored a point.") */
+            } else if (roundResult == 1){
+                const playerDrewText = document.createElement('p');
+                playerDrewText.classList.add('playerDrewText')
+                playerDrewText.textContent = "You drew.";
+                resultOutput.appendChild(playerDrewText);
+    
+                /* console.log("You drew.") */
+            } else {
+                computerScore++
+                const playerLostText = document.createElement('p');
+                playerLostText.classList.add('playerLostText')
+                playerLostText.textContent = "You lost.";
+                resultOutput.appendChild(playerLostText);
+    
+                /* console.log("The computer won and scored a point.") */
+            }
+    
+            const scoreText = document.createElement('p');
+            scoreText.classList.add('scoreText')
+            scoreText.textContent = `Your score is ${playerScore} and the computers is ${computerScore}.`;
+            resultOutput.appendChild(scoreText);
+    
+            console.log(`Your score is ${playerScore} and the computers is ${computerScore}`)
+            console.log(playerSelection)
+            }
         }
+    
 
-        const scoreText = document.createElement('p');
-        scoreText.classList.add('scoreText')
-        scoreText.textContent = `Your score is ${playerScore} and the computers is ${computerScore}.`;
-        resultOutput.appendChild(scoreText);
 
-        console.log(`Your score is ${playerScore} and the computers is ${computerScore}`)
-        }
-    }
+/* Problemet är att spelet (funktionen game()) startar genom att man trycker på en av knapparna.
+I och med att det inte finns något i game() som gör att man någonsin igen behöver trycka på en knapp, körs alla rundor igenom
+utan att spelare får göra val för runda 2-5
 
+Vad behövs?
+
+game() längst ned
+
+lägg in condition för om if loopen ska börja rulla. ska inte starta om player select är tom. sedan när första rundan är över så sätter den player select till tom igen
+
+*/
