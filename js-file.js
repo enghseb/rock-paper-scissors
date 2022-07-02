965cd7e0a0726eb2e821c0f54d4e9d2fff019bf9
Sebastian <enghseb@gmail.com>
2022-07-02 17:44:10 +0000

    Refactored basically all of the code, added comments got rounds to work as well.

--- js-file.js
+++ js-file.js
@@ -1,38 +1,15 @@
-const buttons = document.querySelectorAll('button');
-buttons.forEach((button) => {
+//Variables needed for the game
+playerScore = 0
+computerScore = 0
+rounds = 0
 
-    //Listens for button click
-    button.addEventListener('click', () => {
-    playerSelection = button.id
-
-    //Adds a class to playerAnimation that starts it
-    const playerKey = document.querySelector(`.playerAnimation`)
-    const compKey = document.querySelector('.compAnimation')
-
-    //Changes the ending background image for player to button.id that was retrieved
-    document.documentElement.style
-    .setProperty('--end-background-image', `url(images/${button.id}.png)`);
-
-
-
-    /* Had to remove instead of using "playerAnimation playing",
-    that ended up forcing the animation back to "playing"
-    state after animation was done */
-    playerKey.classList.remove('playerAnimation')
-    playerKey.classList.add(`playing`)
-    compKey.classList.remove('compAnimation')
-    compKey.classList.add(`compPlaying`)
-
-
-    console.log(playerKey)
-    game()
-});
-});
-
-
-const resultOutput = document.querySelector('#result')
+function delay(time) {
+    /* Makes it possible to delay time by xxxx ms */
+    return new Promise(resolve => setTimeout(resolve, time));
+  }
 
 function computerPlay(){
+    /* Randomizes the computers hand to either rock, scissors or paper */
     randomNumber =  Math.random() * 3;
     //Used to get a number without decimals
     randomNumberFloored = Math.floor(randomNumber)
@@ -46,91 +23,91 @@ function computerPlay(){
     }
 
    return computerSelection;
-    
-}
-
-function userPlay(){
-    let userInput = playerSelection;
-    return userInput;
 }
 
 
-function playRound(computerPlay, userPlay){
-    if (userPlay == "rock" && computerPlay == "scissors" || userPlay == "scissors" && computerPlay == "paper" || userPlay == "paper" && computerPlay == "rock") {
-        result = 0
-    }   else if (userPlay == "rock" && computerPlay == "rock" || userPlay == "scissors" && computerPlay == "scissors" || userPlay == "paper" && computerPlay == "paper") {
-        result = 1
+function playRound(computerPlay, playerSelection){
+    /* Plays out the winner of the round and returns the score*/
+    rounds++
+    if (playerSelection == "rock" && computerPlay == "scissors" || playerSelection == "scissors" && computerPlay == "paper" || playerSelection == "paper" && computerPlay == "rock") {
+        console.log("Test")
+        return 0
+    }   else if (playerSelection == "rock" && computerPlay == "rock" || playerSelection == "scissors" && computerPlay == "scissors" || playerSelection == "paper" && computerPlay == "paper") {
+        return 1
     }   else {
-        result = 2
+        return 2
+    }
+}
+
+function score(result){
+    /*Checks the score, prints correct data for score
+    and updates variables so the score is correct */
+    const resultOutput = document.querySelector('#result')
+
+    if(result == 0) {
+        playerScore++
+        const   playerWonText = document.createElement('p');
+        playerWonText.classList.add('playerWonText')
+        playerWonText.textContent = "You won and scored a point.";
+        resultOutput.appendChild(playerWonText);
+    } else if (result == 1){
+        const playerDrewText = document.createElement('p');
+        playerDrewText.classList.add('playerDrewText')
+        playerDrewText.textContent = "You drew.";
+        resultOutput.appendChild(playerDrewText);
+    } else {
+        computerScore++
+        const playerLostText = document.createElement('p');
+        playerLostText.classList.add('playerLostText')
+        playerLostText.textContent = "You lost.";
+        resultOutput.appendChild(playerLostText);
     }
 
-    // Makes the background image become the correct hand for computer, after animation has ended
-    document.documentElement.style
-    .setProperty('--compEnd-background-image', `url(images/${computerSelection}_flipped.png)`);
-
-    const BothChoicesText = document.createElement('p');
-    BothChoicesText.classList.add('BothChoicesText')
-    BothChoicesText.textContent = `You go for a ${userPlay} and the computer opts for ${computerPlay}`;
-    resultOutput.appendChild(BothChoicesText);
-
-    /* console.log(`You go for a ${userPlay} and the computer opts for ${computerPlay}`) */
-    return result;
-
+console.log(playerScore)
+console.log(computerScore)
+console.log(rounds)
+/* console.log(playerSelection) */
 }
 
-function game(){
-    let playerScore = 0
-    let computerScore = 0
-    /* if(playerSelection != "") { */
-        for (let rounds = 0; rounds < 1; rounds++) {
-            roundResult = playRound(computerPlay(), userPlay())
+//Game
+    const buttons = document.querySelectorAll('button');
+    buttons.forEach((button) => {
 
-            if (roundResult == 0){
-                playerScore++;
-    
-                const playerWonText = document.createElement('p');
-                playerWonText.classList.add('playerWonText')
-                playerWonText.textContent = "You won and scored a point.";
-                resultOutput.appendChild(playerWonText);
-    
-                /* console.log("You won and scored a point.") */
-            } else if (roundResult == 1){
-                const playerDrewText = document.createElement('p');
-                playerDrewText.classList.add('playerDrewText')
-                playerDrewText.textContent = "You drew.";
-                resultOutput.appendChild(playerDrewText);
-    
-                /* console.log("You drew.") */
-            } else {
-                computerScore++
-                const playerLostText = document.createElement('p');
-                playerLostText.classList.add('playerLostText')
-                playerLostText.textContent = "You lost.";
-                resultOutput.appendChild(playerLostText);
-    
-                /* console.log("The computer won and scored a point.") */
-            }
-    
-            const scoreText = document.createElement('p');
-            scoreText.classList.add('scoreText')
-            scoreText.textContent = `Your score is ${playerScore} and the computers is ${computerScore}.`;
-            resultOutput.appendChild(scoreText);
-    
-            console.log(`Your score is ${playerScore} and the computers is ${computerScore}`)
-            console.log(playerSelection)
-            }
-        }
-    
+    //Listens for button click
+    button.addEventListener('click', () => {
 
+        //Queries the player/comp hand images, then changes class so that the animation begins
+        const playerKey = document.querySelector(`.playerAnimation`)
+        const compKey = document.querySelector('.compAnimation')
+        playerKey.classList.remove('playerAnimation')
+        playerKey.classList.add(`playing`)
+        compKey.classList.remove('compAnimation')
+        compKey.classList.add(`compPlaying`)
 
-/* Problemet är att spelet (funktionen game()) startar genom att man trycker på en av knapparna.
-I och med att det inte finns något i game() som gör att man någonsin igen behöver trycka på en knapp, körs alla rundor igenom
-utan att spelare får göra val för runda 2-5
+        //Makes the ending of the animation show the correct image according to button pressed/computer hand
+        document.documentElement.style
+        .setProperty('--end-background-image', `url(images/${button.id}.png)`);
+        
+        //Plays a round with the players selection, and the outcome of the function computer play
+        playerSelection = button.id
+        result = playRound(computerPlay(), playerSelection)
+        //Runs the result through the score function
+        score(result)
 
-Vad behövs?
+        //Makes sure we can play more than one round
+        const animated = document.querySelector('.playing');
+        animated.addEventListener('animationend', () => {
+            console.log(animated)
+            //Delay has to be used, otherwise the animation gets cut off and we can't see the result
+            delay(1500).then(() => {
+            //Resets the start position of divs making, putting the hands back to start position
+            playerKey.classList.remove('playing')
+            playerKey.classList.add(`playerAnimation`);
+            compKey.classList.remove('compPlaying')
+            compKey.classList.add(`compAnimation`)
+        });
 
-game() längst ned
-
-lägg in condition för om if loopen ska börja rulla. ska inte starta om player select är tom. sedan när första rundan är över så sätter den player select till tom igen
-
-*/
+        });
+    
+});
+});
