f1b800bc34afcef81ae0cc557d5a9af04c3eb457
Sebastian <enghseb@gmail.com>
2022-07-03 09:02:01 +0000

    Cleaned code, added functions to remove some repetition

--- js-file.js
+++ js-file.js
@@ -5,7 +5,7 @@ rounds = 0
 winner = 0
 
 function delay(time) {
-    /* Makes it possible to delay time by xxxx ms */
+    /* Makes it possible to delay time by xx ms */
     return new Promise(resolve => setTimeout(resolve, time));
   }
 
@@ -26,7 +26,6 @@ function computerPlay(){
    return computerSelection;
 }
 
-
 function playRound(computerPlay, playerSelection){
     /* Plays out the winner of the round and returns the score*/
     rounds++
@@ -45,11 +44,18 @@ function playRound(computerPlay, playerSelection){
 }
 
 function changeTextContent(elementById, textContent) {
+    /* Changes textContent in DOM by ID */
     const elementToChange = document.getElementById(elementById);
     elementToChange.textContent = textContent;
 }
+
+function setCssProperty(variable, property) {
+    /*Changes a .css variable */
+    document.documentElement.style
+    .setProperty(variable, property)
+}
     
-function showResult(winner) {
+function setResultBoxVariable(winner) {
     /* Sets variables for what the resultbox should say */
     if(winner == 0) {
         textContentRock = "YOU"
@@ -64,94 +70,78 @@ function showResult(winner) {
         textContentPaper = "GOT A"
         textContentScissors = "POINT"
     }
-
 }
 
-
+function setResultBoxText(setting) {
+    /* Updates the result box. 
+    Setting 0 = default (show the default playing buttons)
+    Setting 1 = set to variable (show result) */
+    if(setting == 1){
+        changeTextContent("rock", textContentRock)
+        changeTextContent("paper", textContentPaper)
+        changeTextContent("scissors", textContentScissors)
+    } else {
+        changeTextContent("rock", "Rock")
+        changeTextContent("paper", "Paper")
+        changeTextContent("scissors", "Scissors")
+    }
+}
 
 //Game
+    //Listens for button click
     const buttons = document.querySelectorAll('button');
     buttons.forEach((button) => {
-
-    //Listens for button click
     button.addEventListener('click', () => {
         
         //Buttons become unclickable
-        document.documentElement.style
-        .setProperty('--buttonUnclickable-pointer-events', 'none')
+        setCssProperty('--buttonUnclickable-pointer-events', 'none')
 
         /* Buttons animation gets started, swap color text
         to turn it invisible while it's also changed to
         reflect point or not */
-        document.documentElement.style
-        .setProperty('--buttonColor-color', 'red')
-        document.documentElement.style
-        .setProperty('--buttonStartAnimation-animation', 'animateButton 1s forwards, showResult 1.5s forwards')
-        document.documentElement.style
-        .setProperty('--buttonStartAnimationDelay-animation-delay', '0s, 1.5s, 4s')
+        setCssProperty('--buttonColor-color', 'red')
+        setCssProperty('--buttonStartAnimation-animation', 'animateButton 1s forwards, showResult 1.5s forwards')
+        setCssProperty('--buttonStartAnimationDelay-animation-delay', '0s, 1.5s, 4s')
 
-        
         //Queries the player/comp hand images, then changes class so that the animation begins
         const playerKey = document.querySelector(`.playerAnimation`)
         const compKey = document.querySelector('.compAnimation')
-        playerKey.classList.remove('playerAnimation')
-        playerKey.classList.add(`playing`)
-        compKey.classList.remove('compAnimation')
-        compKey.classList.add(`compPlaying`)
+        playerKey.classList = "playing"
+        compKey.classList = "compPlaying"
         
         //Plays a round with the players selection, and the outcome of the function computer play
         playerSelection = button.id
         computerSelection = computerPlay()
-        console.log(playerSelection)
-        console.log(computerSelection)
         result = playRound(computerSelection, playerSelection)
         
-        //Checks what the resultbox should say and then changes the text
-        showResult(winner)
-        changeTextContent("rock", textContentRock)
-        changeTextContent("paper", textContentPaper)
-        changeTextContent("scissors", textContentScissors)
+        //Sets the correct variables for changing the resultbox and then changes the DOM text
+        setResultBoxVariable(winner)
+        setResultBoxText(1)
 
         //Makes the ending of the animation show the correct image according to button pressed/computer hand
-        document.documentElement.style
-        .setProperty('--end-background-image', `url(images/${button.id}.png)`);
-        document.documentElement.style
-        .setProperty('--compEnd-background-image', `url(images/${computerSelection}_flipped.png)`);
-
-        //Runs the result through the score function, also gets it as var so we can run it through the showResult function
-        /* winner = score(result) */
-        console.log("test")
+        setCssProperty('--end-background-image', `url(images/${button.id}.png)`)
+        setCssProperty('--compEnd-background-image', `url(images/${computerSelection}_flipped.png)`)
 
         //Makes sure we can play more than one round
         const animated = document.querySelector('.playing');
         animated.addEventListener('animationend', () => {
-            console.log(animated)
             //Delay has to be used, otherwise the animation gets cut off and we can't see the result
             delay(1500).then(() => {
             
             //Updates the score number in DOM
-            const playerScoreElement = document.getElementById('score-player');
-            playerScoreElement.textContent = `${playerScore}`;
-            const computerScoreElement = document.getElementById('score-computer');
-            computerScoreElement.textContent = `${computerScore}`;
+            changeTextContent("score-player", `${playerScore}`)
+            changeTextContent("score-computer", `${computerScore}`)
 
             //Resets the start position of divs making, putting the hands back to start position
-            playerKey.classList.remove('playing')
-            playerKey.classList.add(`playerAnimation`);
-            compKey.classList.remove('compPlaying')
-            compKey.classList.add(`compAnimation`)
-            document.documentElement.style
+            playerKey.classList = "playerAnimation";
+            compKey.classList = "compAnimation"
 
             //Turns the result text back into buttons, that are clickable again.
-            .setProperty('--buttonStartAnimation-animation', 'animateButtonBack 1.5s forwards')
-            changeTextContent("rock", "Rock")
-            changeTextContent("paper", "Paper")
-            changeTextContent("scissors", "Scissors")
-            document.documentElement.style
-            .setProperty('--buttonUnclickable-pointer-events', 'auto')
+            setCssProperty('--buttonStartAnimation-animation', 'animateButtonBack 1.5s forwards')
+            setResultBoxText(0)
+            setCssProperty('--buttonUnclickable-pointer-events', 'auto')
             changeTextContent("resultText", `You played ${playerSelection} and the CPU played ${computerSelection}. ${textContentRock} ${textContentPaper} ${textContentScissors}`)
         });
-
         });
     
 });
